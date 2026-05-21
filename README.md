72b92a3f0883a7f58a32c219fab018a41388f080
Geovanna Nunes <geovannanunes349@gmail.com>
2026-05-21 00:16:12 +0000

    20/05

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 git add . <br>
 git commit -m "códigos da aula do dia 26/02" <br>
-git push origin main
+git push origin main 
---
+++ js/ex2.js
@@ -0,0 +1,23 @@
+/***function soma(nome){
+    return "Ola, " + nome + "!";
+}
+
+let frase = saudacao("Geovanna");
+
+console.log(frase);
+
+
+function soma(a,b){
+    return a+b;
+}
+
+let resultado = soma(4 , 4);
+console.log("O resultado da soma é: " + resultado);
+***/
+
+function soma(a,b){
+    return a*b;
+}
+
+let resultado = soma(4 , 4);
+console.log("O resultado da multiplicação é: " + resultado);
